02c1dda46ee56d0aaace1b8400a0627134933547
GeoCam <geocam@local.dev>
2026-03-28 08:18:53 +0900

    fix: revert vite base to / for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: '/geostudio-console/',
+  base: '/',
   server: {
     port: 5173,
     proxy: {
